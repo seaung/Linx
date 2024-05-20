8ad8cbe60b4f12fdf87f33dc83ab9226a18cccc0
seaung <seaung.c0m7de@protonmail.com>
2024-05-21 00:26:34 +0800

    重新写

--- lib/tools/utils.py
+++
@@ -1,6 +0,0 @@
-import termcolor
-
-def colors(message, color):
-    msg = termcolor.colored(str(message), str(color), attrs=["bold"])
-    return msg
-
--- lins.py
+++ lins.py
@@ -1,24 +1,17 @@
 import sys
 import os
 
-from lib.core.processer import Processer
-from config.ascii import show_banner
 
+version = "1.0.0"
 
-version = "0.1.0"
-#author = "seaung"
-author = "70ty"
-
-p = Processer()
+author = "seaung"
 
 
 if __name__ == "__main__":
     if os.geteuid() != 0:
         sys.exit("[!] Only for roots kido!")
     try:
-        print(show_banner())
-        print()
-        p.start()
+        pass
     except Exception as e:
-        #print("[!] Exception Unkonow Error : {}".format(e))
         print("[!] Exception caught : {}".format(e))
+
